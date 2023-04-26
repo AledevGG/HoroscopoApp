bebda9db5dff727c48c26c487ad69c475551cc26
alex <thelasttimeale@gmail.com>
2023-04-26 12:59:41 -0300

    Implemented Dagger Hilt

---
+++ app/src/main/java/com/master/horoscopoapp/ui/Horoscopoapp.kt
@@ -0,0 +1,7 @@
+package com.master.horoscopoapp.ui
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Horoscopoapp: Application()
--- app/src/main/java/com/master/horoscopoapp/ui/informations/InformationsFragment.kt
+++ app/src/main/java/com/master/horoscopoapp/ui/informations/InformationsFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.master.horoscopoapp.R
+import com.master.horoscopoapp.ui.list.ListViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class InformationsFragment : Fragment() {
 
-
+    private val viewModel by viewModels<InformationsViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
